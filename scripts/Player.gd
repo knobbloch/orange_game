bb639343896fa214719a9f7d758f769036fb2b43
knobbloch <79453359+knobbloch@users.noreply.github.com>
2023-01-09 21:44:30 +0500

    half of sim-city

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody
 
-const MOUSE_SENSIVITY: float = 0.2
-const MOVE_SPEED: float = 3.0
+const MOUSE_SENSIVITY: float = 0.13
+const MOUSE_SENSIVITY_X: float = 0.13
+const MOUSE_SENSIVITY_Y: float = 0.2
+const MOVE_SPEED: float = 5.0
 const GRAVITY_ACCELERATION: float = 9.8
-const JUMP_FORCE: float = 3.0
+const JUMP_FORCE: float = 2.0
 onready var look_pivot: Spatial = $LookPivot
 # Declare member variables here. Examples:
 # var a = 2
@@ -16,8 +18,8 @@ func _ready():
 	
 func _input (event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg2rad(-1 * event.relative.x * MOUSE_SENSIVITY))
-		look_pivot.rotate_x(deg2rad(event.relative.y) * MOUSE_SENSIVITY)
+		rotate_y(deg2rad(-1 * event.relative.x * MOUSE_SENSIVITY_Y))
+		look_pivot.rotate_x(deg2rad(event.relative.y) * MOUSE_SENSIVITY_X)
 		look_pivot.rotation.x = clamp(look_pivot.rotation.x, deg2rad (-90), deg2rad (90))
 
 func _physics_process(delta):
@@ -25,9 +27,18 @@ func _physics_process(delta):
 	if not is_on_floor():
 		gravity_local += GRAVITY_ACCELERATION * Vector3.DOWN * delta
 	else:
-		gravity_local = GRAVITY_ACCELERATION * get_floor_normal() * delta
-	if Input.is_action_just_pressed("jump"):
+		gravity_local = GRAVITY_ACCELERATION * -get_floor_normal() * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		gravity_local = Vector3.UP * JUMP_FORCE
+		
+	if Input.is_action_just_pressed("interaction"):
+		if $LookPivot/Camera/InteractionRay.is_colliding():
+			var x = $LookPivot/Camera/InteractionRay.get_collider()
+			print("COLLIDING")
+			print(x.get_name())
+			#if x.has_method("interact"):
+			x.interact()
+		
 	move_and_slide(input_move + gravity_local * MOVE_SPEED, Vector3.UP)
 	
 func get_input_direction() -> Vector3:
