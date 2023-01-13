a85b3d56f6c1705cacd3129fb8f8e4f53cbf8560
llnnly <llnnnlly@gmail.com>
2023-01-13 13:14:48 +0500

    delete all because of yes

--- scripts/grandpa.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody
-
-func _ready():
-	pass # Replace with function body.
-
-func interact(inventory, targets, dialogue):
-	print("HIII")
-	inventory.addItemToInventory("water_jar")
-
