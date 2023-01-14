2482a2ce7b28dbda4270b5fdaa87603b7497fe49
Facenip <122119233+Facenip@users.noreply.github.com>
2023-01-14 15:22:34 +0700

    Changed models

--- scripts/grandpa.gd
+++ scripts/grandpa.gd
@@ -1,22 +1,19 @@
 extends StaticBody
 
-
-var is_dualogue_continue=false
+var is_dualogue_continue=0
 
 func _ready():
 	pass
 
 
 func interact(inventory, targets, dialogue):
-	get_tree().change_scene("res://scenes/ClsCutScene.tscn")
 	inventory.addMoneyToInventory()
 	dialogue.SpeakerIs("Дед")
-	if(!is_dualogue_continue):
+	if(is_dualogue_continue==0):
 		dialogue.StartDialogue()
-		is_dualogue_continue=true
-		dialogue.ChangeTextTo("ХАХА,Я НЕ ДЕД")
+		is_dualogue_continue=1
+		dialogue.ChangeTextTo("Хррррр… Хррр… Хр…")
 	else:
 		dialogue.CloseDialogue()
-		is_dualogue_continue=false
-	
+		is_dualogue_continue=0
 
