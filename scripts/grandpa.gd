29b5f5bd149a5767f23db99c9f8a34fd0fb6f1a8
knobbloch <79453359+knobbloch@users.noreply.github.com>
2023-01-14 17:02:52 +0500

    2

--- scripts/grandpa.gd
+++ scripts/grandpa.gd
@@ -1,18 +1,22 @@
 extends StaticBody
 
-var is_dualogue_continue=0
+
+var is_dualogue_continue=false
 
 func _ready():
 	pass
 
 
 func interact(inventory, targets, dialogue):
+	#get_tree().change_scene("res://scenes/ClsCutScene.tscn")
+	inventory.addMoneyToInventory()
 	dialogue.SpeakerIs("Дед")
-	if(is_dualogue_continue==0):
+	if(!is_dualogue_continue):
 		dialogue.StartDialogue()
-		is_dualogue_continue=1
-		dialogue.ChangeTextTo("Хррррр… Хррр… Хр…")
+		is_dualogue_continue=true
+		dialogue.ChangeTextTo("ХАХА,Я НЕ ДЕД")
 	else:
 		dialogue.CloseDialogue()
-		is_dualogue_continue=0
+		is_dualogue_continue=false
+	
 
