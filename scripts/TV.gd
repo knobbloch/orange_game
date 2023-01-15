a01d5e1925c30b0a18acf59dafcac1dc63f74c0c
Facenip <122119233+Facenip@users.noreply.github.com>
2023-01-15 23:43:09 +0700

    121

--- scripts/TV.gd
+++ scripts/TV.gd
@@ -1,19 +1,11 @@
-extends StaticBody
-
-var STOP=1
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+tool extends StaticBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func interact(inventory, targets, dialogue):
-	if GLOBAL.isJarPutted && STOP==1:
-		STOP+=1
+	if GLOBAL.isJarPutted:
+		#ResourceSaver.save("res://path/name.scn", )
+		#get_tree().change_scene_to("")
 		GLOBAL.nextScene('ClsCutScene')
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
